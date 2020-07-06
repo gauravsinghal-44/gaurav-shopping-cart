35178341a0333ef6f47ef90ac682692f62e407a1
gauravsinghal-44 <67220399+gauravsinghal-44@users.noreply.github.com>
2020-07-07 00:56:36 +0530

    changes..

--- server.js
+++ server.js
@@ -9,3 +9,5 @@ app.use(express.static(__dirname + '/dist/shopping-cart'));
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/shopping-cart/index.html'));
 });
+
+app.listen(process.env.PORT || 8080);
