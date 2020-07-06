0f4fc4398946f113818d394ac1b1fe81767e230d
gauravsinghal-44 <67220399+gauravsinghal-44@users.noreply.github.com>
2020-07-06 23:14:57 +0530

    changes...

---
+++ src/app/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files....
+app.use(express.static(__dirname + '/dist/shopping-cart'));
+
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/shopping-cart/index.html'));
+});
